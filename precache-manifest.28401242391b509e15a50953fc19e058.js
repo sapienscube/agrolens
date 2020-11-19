a59a7037a565de5dad1dcc5a5e0a21d11b6483b4
Moctar Diallo <moctarjallo@gmail.com>
2020-11-19 13:15:17 +0100

    Updates

---
+++ precache-manifest.28401242391b509e15a50953fc19e058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f8f85f4969df28ee2f45cd1d2488a6",
+    "url": "/agrolens/index.html"
+  },
+  {
+    "revision": "73ac16c73efbf5b263e2",
+    "url": "/agrolens/static/css/main.4f5a973e.chunk.css"
+  },
+  {
+    "revision": "444b227a067be4e6d344",
+    "url": "/agrolens/static/js/2.db734459.chunk.js"
+  },
+  {
+    "revision": "4ea1de5b4112d7824e896b0cb5fa5f7a",
+    "url": "/agrolens/static/js/2.db734459.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73ac16c73efbf5b263e2",
+    "url": "/agrolens/static/js/main.f103ceef.chunk.js"
+  },
+  {
+    "revision": "286db79274479d73b4a5",
+    "url": "/agrolens/static/js/runtime-main.854cdac4.js"
+  }
+]);
